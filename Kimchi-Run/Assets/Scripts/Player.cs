2e8694b668fe205282fd8fadf2c854c6a3714d02
devgame2020 <scym12@gmail.com>
2025-03-17 21:55:27 +0900

    김치런 프로젝트 : KillPlayer() 가 두번 호출되는 버그 수정

--- Kimchi-Run/Assets/Scripts/Player.cs
+++ Kimchi-Run/Assets/Scripts/Player.cs
@@ -43,6 +43,7 @@ public class Player : MonoBehaviour
 
     public void KillPlayer()
     {
+        if (PlayerCollider.enabled == false) return; 
         PlayerCollider.enabled = false;
         PlayerAnimator.enabled = false;
         PlayerRigidBody.AddForceY(JumpForce, ForceMode2D.Impulse);
